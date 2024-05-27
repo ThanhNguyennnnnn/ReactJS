0d0a444614f54c768d5f18178fb4a3b184eca980
Thanh Nguyen <viechoctap6789@gmail.com>
2024-05-27 13:21:55 +0700

    82

---
+++ src/containers/Patient/Doctor/BookingModal.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import './BookingModal.scss';
+import { FormattedMessage } from 'react-intl';
+import { Modal } from 'reactstrap';
+import { classNames } from 'react-select/dist/index-ea9e225d.cjs.prod';
+
+class BookingModal extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    async componentDidMount() {
+
+    }
+
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.language !== prevProps.language) {
+
+        }
+
+    }
+
+
+
+    render() {
+        let { isOpenModal, closeBookingClose, dataTime } = this.props
+
+        return (
+            //toggle={} 
+            <div>
+                <Modal isOpen={isOpenModal} className={'booking-modal-container'}
+                    size='lg'
+                    centered
+                // backdrop={true}
+                >
+                    <div className='booking-modal-content'>
+                        <div className='booking-modal-header'>
+                            <span className='left'>
+                                Thong tin dat lich kham benh
+                            </span>
+                            <span className='right'
+                                onClick={closeBookingClose}
+                            >
+                                <i className='fas fa-times'></i>
+                            </span>
+                        </div>
+                        <div className='booking-modal-body'>
+                            {/* {JSON.stringify(dataTime)} */}
+                            <div className='doctor-infor'>
+
+                            </div>
+                            <div className='price'>
+                                Gia kham 500.000 VND
+                            </div>
+                            <div className='row'>
+                                <div className='col-6 form-group'>
+                                    <label>Ho Ten</label>
+                                    <input className='form-control' />
+                                </div>
+                                <div className='col-6 form-group'>
+                                    <label>So dien thoai</label>
+                                    <input className='form-control' />
+                                </div>
+                                <div className='col-6 form-group'>
+                                    <label>Dia chi email</label>
+                                    <input className='form-control' />
+                                </div>
+                                <div className='col-6 form-group'>
+                                    <label>Dia chi lien he</label>
+                                    <input className='form-control' />
+                                </div>
+                                <div className='col-12 form-group'>
+                                    <label>Ly do kham benh</label>
+                                    <input className='form-control' />
+                                </div>
+                                <div className='col-6 form-group'>
+                                    <label>Dat cho ai</label>
+                                    <input className='form-control' />
+                                </div>
+                                <div className='col-6 form-group'>
+                                    <label>Gioi tinh</label>
+                                    <input className='form-control' />
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className='booking-modal-footer'>
+                            <button
+                                className='btn-booking-confirm'
+                                onClick={closeBookingClose}
+                            >
+                                Xac nhan
+                            </button>
+                            <button
+                                className='btn-booking-cancel'
+                                onClick={closeBookingClose}
+                            >
+                                Huy
+                            </button>
+                        </div>
+                    </div>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookingModal);
